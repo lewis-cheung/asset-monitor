f27894cc637bc0227f513af3cbf8e0c2f051b153
Lewis Cheung <lewislunsj@gmail.com>
2024-07-10 14:50:40 -0400

    chore: typings

---
+++ typings/lib/asset-scanners/near/index.d.ts
@@ -0,0 +1,1 @@
+export { default as NearNativeTokenScanner } from "./near-native.js";
---
+++ typings/lib/asset-scanners/near/near-native.d.ts
@@ -0,0 +1,4 @@
+export default class NearNativeTokenScanner extends BaseNearAssetScanner {
+    /** @type {number} */ static assetDecimals: number;
+}
+import BaseNearAssetScanner from "./base.js";
