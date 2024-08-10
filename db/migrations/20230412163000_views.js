94f3a8a8e062975fb92e528353307c11da31a2f9
Lewis Cheung <lewislunsj@gmail.com>
2024-08-09 22:12:27 -0400

    fix: migrations

---
+++ db/migrations/20230412163000_views.js
@@ -0,0 +1,15 @@
+/**
+ * @param {import('knex').Knex} knex
+ * @returns {Promise<void>}
+ */
+export async function up(knex) {
+	// removed
+}
+
+/**
+ * @param {import('knex').Knex} knex
+ * @returns {Promise<void>}
+ */
+export async function down(knex) {
+	// removed
+}
--- db/migrations/20240710154500_scanner_config_table.js
+++ db/migrations/20240710154500_scanner_config_table.js
@@ -14,13 +14,14 @@ export async function up(knex) {
 		t.increments('id').primary()
 		t.string('chain', 255).notNullable()
 		t.string('type', 255).notNullable()
-		t.string('endpoint', 255).notNullable()
+		t.string('endpoint', 255)
 		t.string('api_key', 255)
 		t.string('rate_limiter_key', 255)
 		t.boolean('is_enabled').notNullable().defaultTo(true)
 		t.timestamp('created_at').notNullable().defaultTo(knex.raw('CURRENT_TIMESTAMP'))
 		t.timestamp('updated_at').notNullable().defaultTo(knex.raw('CURRENT_TIMESTAMP'))
 	})
+	knex.raw(getOnUpdateTriggerSql('asset_scanner_configs'))
 
 	await knex.schema.createTable('asset_infos', t => {
 		t.increments('id').primary()
@@ -33,9 +34,7 @@ export async function up(knex) {
 
 		t.unique(['chain', 'code'])
 	})
-
 	knex.raw(getOnUpdateTriggerSql('asset_infos'))
-	knex.raw(getOnUpdateTriggerSql('asset_scanner_configs'))
 }
 
 /**
@@ -45,5 +44,6 @@ export async function up(knex) {
 export async function down(knex) {
 	Model.knex(knex)
 
+	await knex.schema.dropTableIfExists('asset_infos')
 	await knex.schema.dropTableIfExists('asset_scanner_configs')
 }
