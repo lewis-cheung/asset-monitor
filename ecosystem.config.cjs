bdf9d23b215d11d25c0f67a617099425ba345876
Lewis Cheung <lewislunsj@gmail.com>
2024-08-21 21:38:26 -0400

    fix: increased memory limit

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
 	apps : [{
 		name: 'asset-monitor',
 		script: 'node --require=suppress-experimental-warnings ./cli monitor \'0 * * * *\'',
-		max_memory_restart: '256M',
+		max_memory_restart: '512M',
 		combine_logs: true,
 	}],
 }
