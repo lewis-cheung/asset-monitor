5b356385e44432ac1124de0d527fd9d31a9eddc0
Lewis Cheung <lewislunsj@gmail.com>
2025-03-30 20:20:01 -0400

    fix: check if npm version type is provided

--- npm-version-push.sh
+++ npm-version-push.sh
@@ -1,4 +1,10 @@
 # /bin/bash
 
+# Show npm version --help if missing version type
+if [ -z "$1" ]; then
+  npm version --help
+  exit 1
+fi
+
 VERSION="$(npm version $1)"
 git push && git push origin $VERSION
